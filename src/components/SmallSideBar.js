b460cca41095ac145bef25c9bac3bdaa11349a05
Manish <manisksing@gmail.com>
2024-02-21 00:05:56 +0530

    added component for smallSideBar

--- src/components/SmallSideBar.js
+++ src/components/SmallSideBar.js
@@ -1,9 +1,25 @@
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import { UseSelector, useDispatch } from 'react-redux';
+import { FaTimes } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
 
 const SmallSideBar = () => {
-  return <Wrapper>
-    <h2>SmallSideBar</h2>
-  </Wrapper>
-}
+  return (
+    <Wrapper>
+      <div className="sidebar-container show-sidebar ">
+        <div className="content">
+          <button className="close-btn" onClick={() =>  console.log("toggle") }>
+            <FaTimes/>
+          </button>
+          <header>
+            <Logo/>
+          </header>
+          <div className='nav-links'>nav links</div>
+        </div>
+      </div>
+    </Wrapper>    
+  );
+;}
 
 export default SmallSideBar;
